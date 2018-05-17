Signifigant motion detection algorithm tuning Fixing issue with gyroscope scaling

src/scripts/node_k66f.py
@@ -7,7 +7,7 @@ import numpy as np
 import rospy
 import serial
 from sensor_msgs.msg import Imu, MagneticField
-from std_msgs.msg import Int16
+from std_msgs.msg import Float32
 
 
 class K66FNode(object):
@@ -25,14 +25,17 @@ class K66FNode(object):
         self._publisher_imu = rospy.Publisher('/imu/data_raw', Imu, queue_size=10)
         self._publisher_magnetic = rospy.Publisher('/imu/mag', MagneticField, queue_size=10)
 
-        rospy.Subscriber("/motor/direction", Int16, self._motor_callback)
+        rospy.Subscriber("/motor/move_angle", Float32, self._motor_callback)
 
-    def _motor_callback(self, steps):
+    def _motor_callback(self, angle):
         self._ser_lock.acquire()
 
-        if steps == 0:
+        angle = angle.data
+
+        if angle == 0.:
             self._ser.write('A;')
         else:
+            steps = int(angle / np.pi * 100)
             self._ser.write('S:%d;' % steps)
 
         self._ser_lock.release()
@@ -107,9 +110,9 @@ class K66FNode(object):
 
         imu.orientation_covariance[0] = -1.
 
-        imu.angular_velocity.x = struct.unpack("h", data[4:6])[0] * np.pi / 180.
-        imu.angular_velocity.y = struct.unpack("h", data[6:8])[0] * np.pi / 180.
-        imu.angular_velocity.z = struct.unpack("h", data[8:10])[0] * np.pi / 180.
+        imu.angular_velocity.x = struct.unpack("h", data[4:6])[0] * 2000./32768. * np.pi / 180.
+        imu.angular_velocity.y = struct.unpack("h", data[6:8])[0] * 2000./32768. * np.pi / 180.
+        imu.angular_velocity.z = struct.unpack("h", data[8:10])[0] * 2000./32768. * np.pi / 180.
 
         # Apply Calibration
         imu.angular_velocity.x += self._gyro_cal['x']

src/scripts/node_object_detector.py
@@ -6,9 +6,11 @@ from cv_bridge import CvBridge
 from threading import Lock
 
 import cv2
+import numpy as np
 import rospy
 from pelicannon.msg import CategorizedRegionOfInterest, CategorizedRegionsOfInterest, AngleDeltaImage
 from sensor_msgs.msg import Image, Imu
+from std_msgs.msg import Float32
 
 from tracker import BodyTrackerPipeline, MotionTrackerPipeline
 
@@ -32,6 +34,7 @@ class ObjectDetectorNode(object):
     def _ros_init(self):
         rospy.init_node('object_detector')
         self._publisher = rospy.Publisher('regions_of_interest', CategorizedRegionsOfInterest, queue_size=10)
+        self._publisher_move = rospy.Publisher('/motor/move_angle', Float32, queue_size=1)
 
         if rospy.get_param('object_detector/debug'):
             self._angle_delta_image_publisher = rospy.Publisher('angle_delta_image', AngleDeltaImage, queue_size=100)
@@ -57,18 +60,20 @@ class ObjectDetectorNode(object):
 
         self._imu_lock.acquire()
 
-        for i in self._imu_queue:
-            a_x += i.angular_velocity.x
-            a_y += i.angular_velocity.y
-            a_z += i.angular_velocity.z
+        if len(self._imu_queue) == 0:
+            self._imu_lock.release()
+            return a_x, a_y, a_z
 
-        a_x /= len(self._imu_queue)
-        a_y /= len(self._imu_queue)
-        a_z /= len(self._imu_queue)
+        rad_velocity = np.zeros((len(self._imu_queue), 3))
+
+        for idx, i in enumerate(self._imu_queue):
+            rad_velocity[idx][0] = i.angular_velocity.x
+            rad_velocity[idx][1] = i.angular_velocity.y
+            rad_velocity[idx][2] = i.angular_velocity.z
 
         self._imu_lock.release()
 
-        return a_x, a_y, a_z
+        return np.max(rad_velocity[:,0]), np.max(rad_velocity[:,1]), np.max(rad_velocity[:,2])
 
     def _camera_callback(self, image):
 
@@ -78,18 +83,17 @@ class ObjectDetectorNode(object):
 
         frame = self._cv_br.imgmsg_to_cv2(image, desired_encoding="passthrough")
 
-        # Create grayscale versions
+        # Create grayscale versions and equalize
         frame_grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        # Does this improve performance? In example code in OpenCV documentation.
         frame_grayscale = cv2.equalizeHist(frame_grayscale)
 
         if rospy.get_param('object_detector/debug'):
+
             self._imu_lock.acquire()
             last_imu = [i for i in self._imu_queue]
             self._imu_lock.release()
 
-            adi = AngleDeltaImage(image, self._last_image, last_imu)
+            adi = AngleDeltaImage(self._last_image, image, last_imu)
             self._angle_delta_image_publisher.publish(adi)
 
         av_x, av_y, av_z = self._compute_angular_velocity()
@@ -97,21 +101,35 @@ class ObjectDetectorNode(object):
         # Send data down pipeline and process results
         regions = []
 
-        if rospy.get_param('object_detector/motion_regions'):
-            motion_regions = self._motion_detector.process_frame(frame_grayscale,
-                                                                 phi=av_z * delta_t
-                                                                 if len(self._imu_queue) != 0 else None)
-            for region in motion_regions:
-                regions.append(
-                    CategorizedRegionOfInterest(x=region.x, y=region.y, w=region.w, h=region.h, category='motion'))
-
         if rospy.get_param('object_detector/body_regions'):
             haarcascade_regions = self._body_tracker.process_frame(frame_grayscale)
             for region in haarcascade_regions:
                 regions.append(
                     CategorizedRegionOfInterest(x=region.x, y=region.y, w=region.w, h=region.h, category='body'))
 
+        if rospy.get_param('object_detector/motion_regions'):
+            motion_regions = self._motion_detector.process_frame(frame_grayscale,
+                                                                 phi=av_z * delta_t
+                                                                 if len(self._imu_queue) != 0 and abs(av_z) > 0.01 else None)
+            for region in motion_regions:
+                regions.append(
+                    CategorizedRegionOfInterest(x=region.x, y=region.y, w=region.w, h=region.h, category='motion'))
+
         self._publisher.publish(CategorizedRegionsOfInterest(regions=regions))
+
+        for region in regions:
+            self._publisher_move.publish(0.)
+
+            region_x_midpoint = (region.x + region.x + region.w) / 2.
+            frame_midpoint = frame.shape[1] / 2
+
+            delta_midpoint = region_x_midpoint - frame_midpoint
+
+            angle_coeff = delta_midpoint / frame_midpoint
+            rot_angle = angle_coeff * 78 * np.pi / 180.
+
+            self._publisher_move.publish(rot_angle)
+            break
 
         time_f = datetime.datetime.now()
 

src/scripts/tracker.py
@@ -112,7 +112,7 @@ class BodyTrackerPipeline(object):
 
 
 class MotionTrackerPipeline(object):
-    def __init__(self, coeff_min_area=0.025, coeff_max_area=1.0):
+    def __init__(self, coeff_min_area=0.025, coeff_max_area=0.5):
         self.frame_initial = None
         self.coeff_min_area = coeff_min_area
         self.coeff_max_area = coeff_max_area
@@ -120,20 +120,25 @@ class MotionTrackerPipeline(object):
         self._cv_br = CvBridge()
         self._publisher_image_debug = rospy.Publisher('cv_peek', Image, queue_size=1)
 
-    def process_frame(self, frame, phi=None, frame_x=10, frame_y=10):
+    def process_frame(self, frame, phi=None, blur=True):
 
         if self.frame_initial is None:
-            self.frame_initial = cv2.GaussianBlur(frame, (5, 5), 0)
+            if blur:
+                self.frame_initial = cv2.GaussianBlur(frame, (5, 5), 0)
+            else:
+                self.frame_initial = frame
             return []
 
         frame_initial = self.frame_initial
-        frame_final = cv2.GaussianBlur(frame, (5, 5), 0)
+        if blur:
+            frame_final = cv2.GaussianBlur(frame, (5, 5), 0)
+        else:
+            frame_final = frame
 
         if phi is not None:
             frame_initial_warped = np.reshape(frame_initial.copy(), (frame.shape[0], frame.shape[1], 1))
 
-            # Needs to be calibrated more
-            dx = int(-2. / 15. * phi * 180. / math.pi)
+            dx = 2.646771 * (180. / np.pi) * phi
 
             it = ImageTransformer(frame_initial_warped)
             frame_initial_warped = it.rotate_along_axis(phi=phi, dx=dx)
@@ -144,9 +149,18 @@ class MotionTrackerPipeline(object):
 
             frame_initial = frame_initial_warped
 
+        else:
+            if rospy.get_param('debug/video_source') == '/pelicannon/image_transform':
+                self._publisher_image_debug.publish(
+                    self._cv_br.cv2_to_imgmsg(frame_initial, encoding="passthrough"))
+
         frame_delta = cv2.absdiff(frame_initial, frame_final)
 
         if phi is not None:
+
+            frame_x = abs(int(dx*2))
+            frame_y = abs(int(dx))
+
             frame_delta[0:frame_y, 0:frame_delta.shape[1]] = 0
             frame_delta[frame_delta.shape[0] - frame_y:frame_delta.shape[0], 0:frame_delta.shape[1]] = 0
 
@@ -156,11 +170,17 @@ class MotionTrackerPipeline(object):
         if rospy.get_param('debug/video_source') == '/pelicannon/image_abs_diff':
             self._publisher_image_debug.publish(self._cv_br.cv2_to_imgmsg(frame_delta, encoding="passthrough"))
 
-        thresh = cv2.threshold(frame_delta, 32, 255, cv2.THRESH_BINARY)[1]
+        if phi is None:
+            thresh = cv2.threshold(frame_delta, 32, 255, cv2.THRESH_BINARY)[1]
+        else:
+            thresh = cv2.threshold(frame_delta, 160, 255, cv2.THRESH_BINARY)[1]
 
         # dilate the thresholded image to fill in holes, then find contours
         # on thresholded image
         thresh = cv2.dilate(thresh, None, iterations=3)
+
+        if rospy.get_param('debug/video_source') == '/pelicannon/image_thresh':
+            self._publisher_image_debug.publish(self._cv_br.cv2_to_imgmsg(thresh, encoding="passthrough"))
 
         (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
                                      cv2.CHAIN_APPROX_SIMPLE)
@@ -175,4 +195,7 @@ class MotionTrackerPipeline(object):
 
         # Store this frame as the next frame initial
         self.frame_initial = frame_final
+
+        #if phi <= 0.005:
         return rectangles
+        return []